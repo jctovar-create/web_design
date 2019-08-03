dcbd349913f5eaed0b61d469026b47f77833c20c
JC Tovar <jctovar.create@gmail.com>
2019-08-03 12:27:06 -0500

    updated animated scroll to section jQuery

--- jctovarcreate/jctovarcreate/jctovarcreate.js
+++ jctovarcreate/jctovarcreate/jctovarcreate.js
@@ -1,4 +1,28 @@
+$(document).ready(function(){
+
+$('a[href^="#"]').on('click', function(event){
+  event.preventDefault();
+
+  var target = this.hash; 
+  var $target = $(target);
+
+  $('html, body').animate({
+    'scrollTop': $target.offset().top
+  }, 1000, function(){
+      window.location.hash = target;
+  }); 
+
+});
+
+
 $("#loadNews").click(function(){
-    $("#loadNews").remove();
-    $("#loadMoreSection").slideDown();
-  });
+  $("#loadNews").remove();
+  $("#loadMoreSection").slideDown();
+});
+
+
+
+
+
+
+});
